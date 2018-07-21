ac1e8a0d31f1c3aeacbb75fa434fdf9a4bbc6d5f
kylinwang <1726060524@qq.com>
2018-07-21 16:07:11 +0800

    'v1.0.0'

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kylinwang
+ * Date: 2018/07/21
+ * Time: 16:01
+ */
+
+require_once './vendor/autoload.php';
+use src\sms\Test;
+Test::hello();
---
+++ src/sms/Test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * composer autoload test
+ *
+ * Created by PhpStorm.
+ * User: kylinwang
+ * Date: 2018/07/21
+ * Time: 15:47
+ */
+
+namespace src\sms;
+
+class Test
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function hello()
+    {
+        echo 'hello world';
+    }
+
+}
